8e4e9bcee2aa5422f2b41d4b37c627a50d7693a9
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2022-05-05 20:09:39 +0200

    A3 ungetestet

--- src/blatt02_gruppe04/src/convert_scan_to_cloud.cpp
+++ src/blatt02_gruppe04/src/convert_scan_to_cloud.cpp
@@ -3,61 +3,82 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/LaserScan.h>
 
+#include <cmath>
+
 ros::Publisher pub;
 
 void cloudCallback(const sensor_msgs::LaserScanConstPtr& scan)
 {
-    sensor_msgs::PointCloud2 cloud;
-    cloud.header.frame_id = "laser";
-    cloud.header.stamp = scan->header.stamp;
-    cloud.header.seq = scan->header.seq; //?
+	sensor_msgs::PointCloud2 cloud;
+	cloud.header.frame_id = "laser";
+	cloud.header.stamp = scan->header.stamp;
+	cloud.header.seq = scan->header.seq; //?
 
 
-    sensor_msgs::PointField field_x;
-    field_x.name = "x";
-    field_x.offset = 0 * sizeof(float);
-    field_x.datatype = sensor_msgs::PointField::FLOAT32;
-    field_x.count = 1;
+	sensor_msgs::PointField field_x;
+	field_x.name = "x";
+	field_x.offset = 0 * sizeof(float);
+	field_x.datatype = sensor_msgs::PointField::FLOAT32;
+	field_x.count = 1;
 
-    sensor_msgs::PointField field_y;
-    field_y.name = "y";
-    field_y.offset = 1 * sizeof(float);
-    field_y.datatype = sensor_msgs::PointField::FLOAT32;
-    field_y.count = 1;
+	sensor_msgs::PointField field_y;
+	field_y.name = "y";
+	field_y.offset = 1 * sizeof(float);
+	field_y.datatype = sensor_msgs::PointField::FLOAT32;
+	field_y.count = 1;
 
-    sensor_msgs::PointField field_z;
-    field_z.name = "z";
-    field_z.offset = 2 * sizeof(float);
-    field_z.datatype = sensor_msgs::PointField::FLOAT32;
-    field_z.count = 1;
+	sensor_msgs::PointField field_z;
+	field_z.name = "z";
+	field_z.offset = 2 * sizeof(float);
+	field_z.datatype = sensor_msgs::PointField::FLOAT32;
+	field_z.count = 1;
 
-    cloud.fields.push_back(field_x);
-    cloud.fields.push_back(field_y);
-    cloud.fields.push_back(field_z);
+	cloud.fields.push_back(field_x);
+	cloud.fields.push_back(field_y);
+	//cloud.fields.push_back(field_z);
 
-    cloud.point_step = 3 * sizeof(float);
+	cloud.point_step = 2 * sizeof(float);
 
-    cloud.width = scan->ranges.size();
-    cloud.height = 1; // Was bedeutet die height?
+	cloud.width = scan->ranges.size();
+	cloud.height = 1;
 
-    cloud.row_step = cloud.width * cloud.point_step;
+	cloud.row_step = cloud.width * cloud.point_step;
 
-    // cloud.data.resize(cloud.point_step * cloud.width);
+	// cloud.data.resize(cloud.point_step * cloud.width);
 
+	for (int i = 0; i < scan->ranges.size(); i++)
+	{
+		float range = scan->ranges.at(i);
 
-    // TODO: Wie in 2.2b die Koordinaten berechnen
+		if (range > scan->range_max || range < scan->range_min)
+		{
+			continue;
+		}
 
+		float angle = scan->angle_min + (i * scan->angle_increment);
 
-    //pub.publish(cloud);
+		float x = range * std::cos(angle);
+		float y = range * std::sin(angle);
+
+		int offset = i * 2;
+
+		float* ptr = (float*) &cloud.data;
+
+		ptr[offset] = x;
+		ptr[offset+1] = y;
+	}
+	
+	pub.publish(cloud);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    ros::NodeHandle nh;
+	ros::init(argc, argv, "convert_scan_to_cloud");
+	ros::NodeHandle nh;
 
-    pub = nh.advertise<sensor_msgs::PointCloud2>("cloud", 1);
+	pub = nh.advertise<sensor_msgs::PointCloud2>("cloud", 1);
 
-    ros::Subscriber sub = nh.subscribe("scan", 1, cloudCallback);
+	ros::Subscriber sub = nh.subscribe("scan", 1, cloudCallback);
 
-    ros::spin();
+	ros::spin();
 }
