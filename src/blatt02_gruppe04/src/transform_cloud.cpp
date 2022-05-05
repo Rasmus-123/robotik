2e2426cb1c1865848a3930eb4281922ca0562c08
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2022-05-05 22:56:20 +0200

    A4 ungetestet

--- src/blatt02_gruppe04/src/transform_cloud.cpp
+++ src/blatt02_gruppe04/src/transform_cloud.cpp
@@ -4,12 +4,35 @@
 
 #include <geometry_msgs/TransformStamped.h>
 
+#include <tf2_ros/transform_listener.h>
+
 ros::Publisher pub;
 
+std::string target_frame;
 
 void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
 {
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
 
+    geometry_msgs::TransformStamped transformStamped;
+
+    try
+    {
+        transformStamped = tfBuffer.lookupTransform(target_frame, "base_link", ros::Time(0));
+    }
+    catch (tf2::TransformException& ex)
+    {
+        ROS_WARN("%s", ex.what());
+        ros::Duration(1.0).sleep();
+        return;
+    }
+
+    sensor_msgs::PointCloud2 res;
+
+    tf2::doTransform(*cloud, res, transformStamped);
+
+    pub.publish(res);
 }
 
 int main(int argc, char** argv)
@@ -21,6 +44,8 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh_p("~");
 
 	pub = nh_p.advertise<geometry_msgs::TransformStamped>("cloud", 1);
+
+    nh_p.param<std::string>("target_frame", target_frame, "base_link");
 
 	ros::Subscriber sub = nh_p.subscribe("cloud", 1, cloudCallback);
 
