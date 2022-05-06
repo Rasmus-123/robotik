6e1c9a843ded1311ef4d916c9ca995fb41c366c1
6a67 <83820531+6a67@users.noreply.github.com>
2022-05-06 15:20:38 +0200

    fix

--- src/blatt02_gruppe04/src/online_line_finder.cpp
+++ src/blatt02_gruppe04/src/online_line_finder.cpp
@@ -12,11 +12,19 @@ std::string dest_node;
 
 
 struct Point {
-    double x,y,z;
+    float x,y,z;
 };
 
-double euklidDist(geometry_msgs::Point a, geometry_msgs::Point b) {
+double euklidDist(Point a, Point b) {
     return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
+
+geometry_msgs::Point toGP(Point p) {
+    geometry_msgs::Point tp;
+    tp.x = p.x;
+    tp.y = p.y;
+    tp.z = p.z;
+    return tp;
 }
 
 
@@ -28,13 +36,13 @@ void markerCallback(const sensor_msgs::PointCloud2::ConstPtr &data) {
 
         visualization_msgs::Marker marker;
         // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "/my_frame";
+        marker.header.frame_id = data->header.frame_id;
         marker.header.stamp = ros::Time::now();
 
         // Set the namespace and id for this marker.  This serves to create a unique ID
         // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "basic_shapes";
-        marker.id = 0;
+        marker.ns = "lines";
+        marker.id = 1;
 
         // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
         marker.type = visualization_msgs::Marker::LINE_LIST;
@@ -42,42 +50,52 @@ void markerCallback(const sensor_msgs::PointCloud2::ConstPtr &data) {
         // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
         marker.action = visualization_msgs::Marker::ADD;
 
-        marker.scale.x = 0.01;
+        marker.scale.x = 1;
         marker.color.r = 1.0;
         marker.color.a = 1.0;
+        marker.pose.orientation.w = 1;
 
 
         //for(int i = 0; i < data->fields.front())
     
-        const geometry_msgs::Point* data_raw = reinterpret_cast<const geometry_msgs::Point*>(&data->data[0]);
+        const Point* data_raw = reinterpret_cast<const Point*>(&data->data[0]);
 
+        k = j;
 
-        int j = 0;
-        while(k < sizeof(data_raw) / sizeof(data_raw[0])) {
+        while(k < data->width - 2) {
+            k++;
             double zaehler = euklidDist(data_raw[j], data_raw[k+1]);
+
             double nenner;
-            for(int i = j; j <= k; i++) {
+            for(int i = j; i <= k; i++) {
                 nenner += euklidDist(data_raw[i], data_raw[i+1]);
             }
-            if(zaehler / nenner >= 1) {
-                if(k >= 1) {
-                    double a = euklidDist(data_raw[k-1], data_raw[k+1]);
-                    double b = euklidDist(data_raw[k-1], data_raw[k]) + euklidDist(data_raw[k], data_raw[k+1]);
+            
+            std::cout << zaehler / nenner << std::endl;
 
-                    if(a / b >= 0.2) {
-                        marker.points.push_back(data_raw[k+1]);
-                    }
+            if(zaehler / nenner >= 1 - 1.0/(k-j)) {
+                double a = euklidDist(data_raw[k-1], data_raw[k+1]);
+                double b = euklidDist(data_raw[k-1], data_raw[k]) + euklidDist(data_raw[k], data_raw[k+1]);
+
+                if(a / b >= 0.8) {
+                    marker.points.push_back(toGP(data_raw[k+1]));
                 }
-            } 
-            k++;       
+            }
+  
         }
-        if(j == k) {
-            break;
+
+        // Only create line if there are at least 3 points covered by it
+        if(marker.points.size() >= 3) {
+            marker_pub.publish(marker);
         }
         
-        j = k;
-
-        marker_pub.publish(marker);
+        // End if j reached the last Element
+        if(j == k) {
+            break;
+        } else {
+            // Continue with j from the last ended line
+            j = k;
+        }
         ros::spinOnce();
         
     }
