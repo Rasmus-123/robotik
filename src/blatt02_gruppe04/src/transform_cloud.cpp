3915921969feddf01a76eea926c5666ad8dd6107
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2022-05-06 21:46:51 +0200

    Checkout Transform_Cloud.cpp from blatt02-rasmus

--- src/blatt02_gruppe04/src/transform_cloud.cpp
+++ src/blatt02_gruppe04/src/transform_cloud.cpp
@@ -1,64 +1,66 @@
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/PointField.h>
 #include <ros/ros.h>
-#include <tf2_ros/transform_listener.h>
-// For some reason, tf2_sensor_msgs was not part of the AUR package ros-noetic-desktop-full
-#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+#include <sensor_msgs/PointCloud2.h>
+
 #include <geometry_msgs/TransformStamped.h>
 
+#include <tf2_ros/transform_listener.h>
 
-ros::Publisher tf_pub;
-std::string dest_node;
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 
 
+ros::Publisher pub;
 
-void tfCallback(const sensor_msgs::PointCloud2::ConstPtr &data) {
-    geometry_msgs::TransformStamped payload;
-    std::string last_topic;
+std::string target_frame;
 
+void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
+{
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
+
     geometry_msgs::TransformStamped transformStamped;
 
-    while(ros::ok()) {
-        try {
-            transformStamped = tfBuffer.lookupTransform(dest_node, data->header.frame_id,
-                                ros::Time(0));
+    // Keep retrying the lookupTransform until it succeeds
+    while (ros::ok())
+    {
+        try
+        {
+            transformStamped = tfBuffer.lookupTransform(target_frame, cloud->header.frame_id, ros::Time(0));
             break;
         }
-        catch (tf2::TransformException &ex) {
-            ROS_WARN("%s",ex.what());
+        catch (tf2::TransformException& ex)
+        {
+            //ROS_WARN("%s", ex.what());
             ros::Duration(1.0).sleep();
             continue;
         }
     }
 
-    sensor_msgs::PointCloud2 data_out;
-    tf2::doTransform(*data, data_out, transformStamped);
+    sensor_msgs::PointCloud2 res;
 
-   
-    tf_pub.publish(data_out);
-    ros::spinOnce();
+    tf2::doTransform(*cloud, res, transformStamped);
+
+    pub.publish(res);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "transform_cloud");
 
-    ros::init(argc, argv, "transform_cloud");
+	ROS_INFO("Hallo");
 
-    ros::NodeHandle n;
-    ros::NodeHandle np("~");
+	ros::NodeHandle nh_p("~");
 
-    np.param<std::string>("dest_name", dest_node, "laser");
+    // Publishing private topic
+	pub = nh_p.advertise<sensor_msgs::PointCloud2>("cloud", 1000);
 
-    np.getParam("dest_name", dest_node);
+    // Private parameter transform_cloud/target_frame
+    nh_p.param<std::string>("target_frame", target_frame, "base_link");
 
-    // publisher
-    tf_pub = np.advertise<sensor_msgs::PointCloud2>("cloud", 1000);
+    ROS_INFO_STREAM("Target_Frame: " << target_frame);
 
+    // Subscribing to private topic from convert_scan_to_cloud
+	ros::Subscriber sub = nh_p.subscribe("/convert_scan_to_cloud/cloud", 1000, cloudCallback);
 
-    // subscriber
-    ros::Subscriber sub = n.subscribe("/convert_scan_to_cloud/cloud", 1000, tfCallback);
-    ros::spin();
-
-    return 0;
+	ros::spin();
 }
