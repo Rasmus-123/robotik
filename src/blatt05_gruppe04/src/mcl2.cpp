56807f00f0b55553018c4f5656541a7b5b1b03f8
6a67 <83820531+6a67@users.noreply.github.com>
2022-06-10 17:34:01 +0200

    [blatt05] fixed last_pose not being set

--- src/blatt05_gruppe04/src/mcl2.cpp
+++ src/blatt05_gruppe04/src/mcl2.cpp
@@ -11,52 +11,58 @@ geometry_msgs::PoseArray pose_array;
 
 void ekfCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose) {
 
-    // current pose quaternion to roll pitch yaw
-    tf::Quaternion q(pose->pose.pose.orientation.x, pose->pose.pose.orientation.y, pose->pose.pose.orientation.z, pose->pose.pose.orientation.w);
-    tf::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
+    if(!last_pose.header.frame_id.empty()) {
 
-    // last pose quaternion to roll pitch yaw
-    tf::Quaternion q2(last_pose.pose.pose.orientation.x, last_pose.pose.pose.orientation.y, last_pose.pose.pose.orientation.z, last_pose.pose.pose.orientation.w);
-    tf::Matrix3x3 m2(q2);
-    double roll2, pitch2, yaw2;
-    m2.getRPY(roll2, pitch2, yaw2);
+        // current pose quaternion to roll pitch yaw
+        tf::Quaternion q(pose->pose.pose.orientation.x, pose->pose.pose.orientation.y, pose->pose.pose.orientation.z, pose->pose.pose.orientation.w);
+        tf::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
 
-    // calculate difference in roll pitch yaw
-    double diff_roll = roll - roll2;
-    double diff_pitch = pitch - pitch2;
-    double diff_yaw = yaw - yaw2;
+        // last pose quaternion to roll pitch yaw
+        tf::Quaternion q2(last_pose.pose.pose.orientation.x, last_pose.pose.pose.orientation.y, last_pose.pose.pose.orientation.z, last_pose.pose.pose.orientation.w);
+        tf::Matrix3x3 m2(q2);
+        double roll2, pitch2, yaw2;
+        m2.getRPY(roll2, pitch2, yaw2);
+
+        // calculate difference in roll pitch yaw
+        double diff_roll = roll - roll2;
+        double diff_pitch = pitch - pitch2;
+        double diff_yaw = yaw - yaw2;
 
 
-    for(int i = 0; i < pose_array.poses.size(); i++) {
-        pose_array.poses[i].position.x += pose->pose.pose.position.x - last_pose.pose.pose.position.x;
-        pose_array.poses[i].position.y += pose->pose.pose.position.y - last_pose.pose.pose.position.y;
-        
+        for(int i = 0; i < pose_array.poses.size(); i++) {
+            pose_array.poses[i].position.x += pose->pose.pose.position.x - last_pose.pose.pose.position.x;
+            pose_array.poses[i].position.y += pose->pose.pose.position.y - last_pose.pose.pose.position.y;
+            
 
-        // pose from pose_array to quaternion
-        tf::Quaternion q3(pose_array.poses[i].orientation.x, pose_array.poses[i].orientation.y, pose_array.poses[i].orientation.z, pose_array.poses[i].orientation.w);
-        tf::Matrix3x3 m3(q3);
-        double roll3, pitch3, yaw3;
-        m3.getRPY(roll3, pitch3, yaw3);
+            // pose from pose_array to quaternion
+            tf::Quaternion q3(pose_array.poses[i].orientation.x, pose_array.poses[i].orientation.y, pose_array.poses[i].orientation.z, pose_array.poses[i].orientation.w);
+            tf::Matrix3x3 m3(q3);
+            double roll3, pitch3, yaw3;
+            m3.getRPY(roll3, pitch3, yaw3);
 
-        // calculate new roll pitch yaw
-        double new_roll = roll3 + diff_roll;
-        double new_pitch = pitch3 + diff_pitch;
-        double new_yaw = yaw3 + diff_yaw;
+            // calculate new roll pitch yaw
+            double new_roll = roll3 + diff_roll;
+            double new_pitch = pitch3 + diff_pitch;
+            double new_yaw = yaw3 + diff_yaw;
 
-        // roll pitch yaw to quaternion
-        tf::Quaternion q4;
-        q4.setRPY(new_roll, new_pitch, new_yaw);
+            // roll pitch yaw to quaternion
+            tf::Quaternion q4;
+            q4.setRPY(new_roll, new_pitch, new_yaw);
 
-        // tf::Quaternion to geometry_msgs::Quaternion for pose from pose_array
-        pose_array.poses[i].orientation.x = q4.x();
-        pose_array.poses[i].orientation.y = q4.y();
-        pose_array.poses[i].orientation.z = q4.z();
-        pose_array.poses[i].orientation.w = q4.w();
+            // tf::Quaternion to geometry_msgs::Quaternion for pose from pose_array
+            pose_array.poses[i].orientation.x = q4.x();
+            pose_array.poses[i].orientation.y = q4.y();
+            pose_array.poses[i].orientation.z = q4.z();
+            pose_array.poses[i].orientation.w = q4.w();
+        }
+
+        pose_pub.publish(pose_array);
+
     }
 
-    pose_pub.publish(pose_array);
+    last_pose = *pose;
 }
 
 void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose) {
@@ -79,8 +85,6 @@ void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose
         pose_random.orientation = pose->pose.pose.orientation;
         pose_array.poses.push_back(pose_random);
     }
-
-    pose_pub.publish(pose_array);
 
     ekfCallback(pose);
 }
