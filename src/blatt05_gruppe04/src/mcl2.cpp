839389a8bdb40e3de908ad400b69d0c423f96ff1
6a67 <83820531+6a67@users.noreply.github.com>
2022-06-10 17:41:54 +0200

    [blatt05] fix

--- src/blatt05_gruppe04/src/mcl2.cpp
+++ src/blatt05_gruppe04/src/mcl2.cpp
@@ -68,24 +68,24 @@ void ekfCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose)
 void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose) {
     ROS_INFO_STREAM("Pose: " << pose->pose.pose.position.x << ", " << pose->pose.pose.position.y << ", " << pose->pose.pose.position.z);
 
-    pose_array = geometry_msgs::PoseArray();
+    if(pose_array.poses.size() == 0) {
+        pose_array.header.stamp = pose->header.stamp;
+        pose_array.header.frame_id = pose->header.frame_id;
 
-    pose_array.header.stamp = pose->header.stamp;
-    pose_array.header.frame_id = pose->header.frame_id;
+        pose_array.poses.push_back(pose->pose.pose);
 
-    pose_array.poses.push_back(pose->pose.pose);
+        // get random poses around the current pose with a sigma of 0.5m
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::normal_distribution<double> dist(0, 0.5);
 
-    // get random poses around the current pose with a sigma of 0.5m
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<double> dist(0, 0.5);
-
-    for (int i = 0; i < 100; i++) {
-        geometry_msgs::Pose pose_random;
-        pose_random.position.x = pose->pose.pose.position.x + dist(gen);
-        pose_random.position.y = pose->pose.pose.position.y + dist(gen);
-        pose_random.orientation = pose->pose.pose.orientation;
-        pose_array.poses.push_back(pose_random);
+        for (int i = 0; i < 100; i++) {
+            geometry_msgs::Pose pose_random;
+            pose_random.position.x = pose->pose.pose.position.x + dist(gen);
+            pose_random.position.y = pose->pose.pose.position.y + dist(gen);
+            pose_random.orientation = pose->pose.pose.orientation;
+            pose_array.poses.push_back(pose_random);
+        }
     }
 
     ekfCallback(pose);
