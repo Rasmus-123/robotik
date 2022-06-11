126135168e46251fa9eed6362992ee341c470084
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2022-06-11 18:36:37 +0200

    [Blatt05] A1b - 4 separate Arrays

--- src/blatt05_gruppe04/exercises/A1b.py
+++ src/blatt05_gruppe04/exercises/A1b.py
@@ -20,19 +20,13 @@ def landmark(array, found: bool):
 
     return array
 
-def move_random(array, steps):
-    """
-    Idee:
-      - 50/50 Chance auf beide Richtungen
-      - Also probs1 = imUhrzeigersinn(probs)
-      - Also probs2 = gegenUhrzeigersinn(probs)
-      - Also probs = probs1 * probs2
-    """
-    clockwise = array[-steps:] + array[:-steps]
-    counterclockwise = array[steps:] + array[:steps]
+def move_clockwise(array, steps):
+    """..."""
+    return array[-steps:] + array[:-steps]
 
-    return [a*b for a, b in zip(clockwise, counterclockwise)]
-
+def move_counterclockwise(array, steps):
+    """..."""
+    return array[steps:] + array[:steps]
 
 def normalize(array):
     """Normalisieren eines Zahlenarrays"""
@@ -65,41 +59,65 @@ print_probabilities(probs)
 
 print("## Schritt 2 - 2 Schritte in zufällige Richtung ##")
 
-probs = move_random(probs, 2)
+probs_cw = move_clockwise(probs, 2)
+probs_ccw = move_counterclockwise(probs, 2)
 
-probs = normalize(probs)
+probs_cw = normalize(probs_cw)
+probs_ccw = normalize(probs_ccw)
 
-print_probabilities(probs)
+print_probabilities(probs_cw)
+print_probabilities(probs_ccw)
 
 ## Schritt 3 - Eine Landmarke ##
 
 print("## Schritt 3 - Eine Landmarke ##")
 
-probs = landmark(probs, True)
+probs_cw = landmark(probs_cw, True)
+probs_cw = normalize(probs_cw)
+probs_ccw = landmark(probs_ccw, True)
+probs_ccw = normalize(probs_ccw)
 
-probs = normalize(probs)
-
-print_probabilities(probs)
+print_probabilities(probs_cw)
+print_probabilities(probs_ccw)
 
 ## Schritt 4 - 4 Schritte in zufällige Richtung ##
 
 print("## Schritt 4 - 4 Schritte in zufällige Richtung ##")
 
-probs = move_random(probs, 4)
+probs_cw_cw = move_clockwise(probs_cw, 4)
+probs_ccw_cw = move_clockwise(probs_ccw, 4)
+probs_cw_cw = normalize(probs_cw_cw)
+probs_ccw_cw = normalize(probs_ccw_cw)
 
-probs = normalize(probs)
+probs_cw_ccw = move_counterclockwise(probs_cw, 4)
+probs_ccw_ccw = move_counterclockwise(probs_ccw, 4)
+probs_cw_ccw = normalize(probs_cw_ccw)
+probs_ccw_ccw = normalize(probs_ccw_ccw)
 
-print_probabilities(probs)
+print_probabilities(probs_cw_cw)
+print_probabilities(probs_cw_ccw)
+print_probabilities(probs_ccw_cw)
+print_probabilities(probs_ccw_ccw)
 
 ## Schritt 5 - Keine Landmarke ##
 
 print("## Schritt 5 - Keine Landmarke ##")
 
-probs = landmark(probs, False)
 
-probs = normalize(probs)
+probs_cw_cw = landmark(probs_cw_cw, False)
+probs_cw_cw = normalize(probs_cw_cw)
+probs_cw_ccw = landmark(probs_cw_ccw, False)
+probs_cw_ccw = normalize(probs_cw_ccw)
 
-print_probabilities(probs)
+probs_ccw_cw = landmark(probs_ccw_cw, False)
+probs_ccw_cw = normalize(probs_ccw_cw)
+probs_ccw_ccw = landmark(probs_ccw_ccw, False)
+probs_ccw_ccw = normalize(probs_ccw_ccw)
+
+print_probabilities(probs_cw_cw)
+print_probabilities(probs_cw_ccw)
+print_probabilities(probs_ccw_cw)
+print_probabilities(probs_ccw_ccw)
 
 ## Result ##
 
@@ -111,10 +129,24 @@ print_probabilities(probs)
 #   -2 +  4 =  2
 #   -2 + -4 = -6
 
-max_prob = max(probs)
+def results(array, first, second):
+    max_prob = max(array)
 
-# Find all Indexes with max_prob (in case multiple fields have the same probability)
-max_indexes = [i for i in range(1, len(probs) + 1) if probs[i-1] == max_prob]
-start_fields = [i - 6 for i in max_indexes]
+    steps = first + second
 
-print("Der Roboter steht mit Wahrscheinlichkeit", round(max_prob, 2), "auf Feld:", max_indexes)
+    # Find all Indexes with max_prob (in case multiple fields have the same probability)
+    max_indexes = [i for i in range(1, len(array) + 1) if array[i-1] == max_prob]
+    start_fields = [i - steps for i in max_indexes]
+
+    print("Der Roboter steht mit Wahrscheinlichkeit", round(max_prob, 2), "auf Feld:", max_indexes)
+    print("Der Startzustand war nach", first, "und", second, "Schritten wahrscheinlich Feld:", start_fields)
+
+results(probs_cw_cw, 2, 4)
+results(probs_cw_ccw, 2, -4)
+results(probs_ccw_cw, -2, 4)
+results(probs_ccw_ccw, -2, -4)
+
+print("Alle zusammen:")
+probs_all = [a * b * c * d for a,b,c,d in zip(probs_cw_cw, probs_cw_ccw, probs_ccw_cw, probs_ccw_ccw)]
+probs_all = normalize(probs_all)
+results(probs_all, 0, 0)
